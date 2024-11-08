e8b0cdfce1d60acb015d69defc87f7f71aba1224
Joshua Gonzales <codingjogo@gmail.com>
2024-11-08 13:24:40 +0800

    feat: Add FormComponent

---
+++ components/FormComponent.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import React from 'react'
+import { Form } from './ui/form';
+import { Button } from './ui/button';
+
+const FormComponent = () => {
+    // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
+
+export default FormComponent
